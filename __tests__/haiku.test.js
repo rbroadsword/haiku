964b49f3cdcdfbd3791141adf5af0cb8a01d689f
Ryan Broadsword <rbroadsword@gmail.com>
2022-05-15 10:50:46 -0700

    passes first test for Haiku class

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -3,7 +3,7 @@ import Haiku from '../src/js/function.js';
 describe('Haiku', () => {
 
   test('should create a haiku object with three lines', () => {
-    const haiku = new Haiku("this is line1","this is line2","this is line2");
+    const haiku = new Haiku("this is line1","this is line2","this is line3");
     expect(haiku.line1).toEqual("this is line1");
     expect(haiku.line2).toEqual("this is line2");
     expect(haiku.line3).toEqual("this is line3");
